a756a62ca98405171f6b8ea830bf302c7980a518
H. Marcelo Muriel <marcelomuriel@gmail.com>
2014-03-19 23:34:49 +0100

    chdir to current path added

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ Created on Mar 19, 2014
 @author: hmuriel
 '''
 from crawler.crawl import Feed
+import os
 
 if __name__ == '__main__':
+    os.chdir(os.path.dirname(__file__))
+    print('path set to', os.getcwd())
     Feed().updateFeeds()
